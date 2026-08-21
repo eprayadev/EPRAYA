ed95419109a30f836750802e7d8ffd5246f43419
eprayadev <eprayadev@gmail.com>
2026-08-21 13:04:15 -0500

    Sdata problem in Overseer solved

--- epraya/base_plot.py
+++ epraya/base_plot.py
@@ -363,7 +363,7 @@ def Overseer(field,counts,lt=51,pol=3,i=0,startl=0,endli=-1,epsilon=(5*10**-6),p
 
   
   '''
-  Sdata=[]
+  Sdata={}
   #Sliders conditions
   hystoria=[]
   maind=len(counts)-1
@@ -393,7 +393,7 @@ def Overseer(field,counts,lt=51,pol=3,i=0,startl=0,endli=-1,epsilon=(5*10**-6),p
       ouarea.clear_output(wait=True)
       if hystoria:
         # Save results
-        Sdata=hystoria[-1]
+        Sdata['one']=hystoria[-1]
         print(f"Data saved")
   savev.on_click(on_save_clicked)
   out=interactive_output(Wrapper,{'field': fixed(field),'count': fixed(counts),'startl': sslider,'endli': eslider,'lt': ltslider,'pol': polslider})
@@ -401,7 +401,7 @@ def Overseer(field,counts,lt=51,pol=3,i=0,startl=0,endli=-1,epsilon=(5*10**-6),p
   #controls.layout=widgets.Layout(width='500px',border='solid 1px #cccccc',padding='10px',margin='20px 0px 0 700px' )
   app_layout=widgets.HBox([controls, out])
   display(controls,out)
-  return Sdata
+  return Sdata['one']
   
 #For tkinter 
 def Sload1(dat,rows,cols): #Loads counts and field data
